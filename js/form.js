94dbd4c1f29351adeccd23695b40c2c1986c7dc6
Maksim Buyanov <maksi_777@bk.ru>
2021-12-27 21:55:23 +0700

    add contact line

--- js/form.js
+++ js/form.js
@@ -1,10 +1,10 @@
 let modal;
+const MAX_CONTACT_COUNT = 10;
 const cross = document.querySelector('.card__cross_icon')
 const inputSurname = document.querySelector('.card__input_surname')
 const inputName = document.querySelector('.card__input_name')
 const inputLastName = document.querySelector('.card__input_last-name')
-const modalId = document.querySelector('.card__id')
-// const addContactButton = document.querySelector('.card__button')
+const addContactButton = document.querySelector('.card__button')
 // const contactType = document.querySelector('.add-new-contact__select')
 // const contactInput = document.querySelector('.add-new-contact__input')
 // const deletContact = document.querySelector('.add-new-contact__close')
@@ -18,80 +18,107 @@ cross.addEventListener('click', closeModal)
 
 
 export function openModal(obj, persone = false) {
-    modal = obj;
-    obj.classList.remove('modal_none');
-    if (persone) {
-        inputName.value = persone.name;
-        inputSurname.value = persone.surname;
-        inputLastName.value = persone.lastName;
-        modalId.textContent = `ID: ${persone.id}`
-        title.textContent = ``
+  modal = obj;
+  obj.classList.remove('modal_none');
+  if (persone) {
+    inputName.value = persone.name;
+    inputSurname.value = persone.surname;
+    inputLastName.value = persone.lastName;
+    title.textContent = `Изменить данные `
+    const modalId = document.createElement('span')
+    modalId.textContent = `ID: ${persone.id}`
+    modalId.classList.add('card__id')
+    title.append(modalId)
 
-        renderContact(persone.contacts)
-    }
+    renderContact(persone.contacts)
+  }
 
 }
 
 function renderContact(arr) {
-    arr.forEach(item => {
-        const line = createContactLine(item);
-        contactBlick.prepend(line);
-    });
+  arr.forEach(item => {
+    const line = createContactLine(item);
+    line.children[line.children.length-1].addEventListener('click', () => {
+
+      const arr = document.querySelectorAll('.add-new-contact__wrapper')
+      let el
+      arr.forEach(item=> {
+        if (item.childNodes[1].value === line.childNodes[1].value) {
+          el = item
+        }
+
+      })
+      el.remove()
+
+    })
+    contactBlick.prepend(line);
+  });
+  if (contactBlick.childElementCount>MAX_CONTACT_COUNT) {
+    contactBlick.children[contactBlick.childElementCount-1].remove()
+  }
 }
 
+addContactButton.addEventListener('click',()=> {
+  const line = createContactLine({value:''})
+  contactBlick.prepend(line)
+  if (contactBlick.childElementCount>MAX_CONTACT_COUNT) {
+    contactBlick.children[contactBlick.childElementCount-1].remove()
+  }
+})
+
 function createContactLine(item) {
-    const wrap = document.createElement('div');
-    wrap.classList.add('add-new-contact__wrapper');
+  const wrap = document.createElement('div');
+  wrap.classList.add('add-new-contact__wrapper');
 
-    const select = document.createElement('select');
-    select.classList.add('add-new-contact__select');
-    const optionTel = createOption('tel', 'Телефон', 'add-new-contact__option')
-    const optionVk = createOption('vk', 'Vk', 'add-new-contact__option')
-    const optionFb = createOption('fb', 'Facebook', 'add-new-contact__option')
-    const optionMail = createOption('mail', 'E-mail', 'add-new-contact__option')
-    const optionOther = createOption('other', 'Другие контакты', 'add-new-contact__option')
-    select.append(optionTel, optionVk, optionFb, optionMail, optionOther);
-    select.value = item.type
-    if (!select.value) {
-        select.value = 'other'
-    }
-    wrap.append(select)
+  const select = document.createElement('select');
+  select.classList.add('add-new-contact__select');
+  const optionTel = createOption('tel', 'Телефон', 'add-new-contact__option')
+  const optionVk = createOption('vk', 'Vk', 'add-new-contact__option')
+  const optionFb = createOption('fb', 'Facebook', 'add-new-contact__option')
+  const optionMail = createOption('mail', 'E-mail', 'add-new-contact__option')
+  const optionOther = createOption('other', 'Другие контакты', 'add-new-contact__option')
+  select.append(optionTel, optionVk, optionFb, optionMail, optionOther);
+  select.value = item.type
+  if (!select.value) {
+    select.value = 'other'
+  }
+  wrap.append(select)
 
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.classList = 'add-new-contact__input';
-    input.value = item.value
-    wrap.append(input)
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.classList = 'add-new-contact__input';
+  input.value = item.value
+  wrap.append(input)
 
-    const button = document.createElement('button');
-    button.classList = 'add-new-contact__close';
-    const cross = document.createElement('span');
-    cross.classList.add('add-new-contact__close-icon', 'icon')
-    button.append(cross);
-    wrap.append(button)
-    
-    return wrap
+  const button = document.createElement('button');
+  button.classList = 'add-new-contact__close';
+  const cross = document.createElement('span');
+  cross.classList.add('add-new-contact__close-icon', 'icon')
+  button.append(cross);
+  wrap.append(button)
+
+  return wrap
 }
 
 function createOption(val, text, classList) {
-    const option = document.createElement('option');
-    option.value = val
-    option.textContent = text
-    option.classList.add(classList);
-    return option
+  const option = document.createElement('option');
+  option.value = val
+  option.textContent = text
+  option.classList.add(classList);
+  return option
 }
 
 export function closeModal() {
-    modal.classList.add('modal_none');
-    clearForm()
+  modal.classList.add('modal_none');
+  clearForm()
 }
 
 function clearForm() {
-    inputSurname.value = '';
-    inputName.value = '';
-    inputLastName.value = '';
-    const arr = document.querySelectorAll('.add-new-contact__wrapper')
-    arr.forEach(node => {
-        node.remove()
-    });
+  inputSurname.value = '';
+  inputName.value = '';
+  inputLastName.value = '';
+  const arr = document.querySelectorAll('.add-new-contact__wrapper')
+  arr.forEach(node => {
+    node.remove()
+  });
 }
